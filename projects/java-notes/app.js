a2dfccc35f26108ca8f9317da1a85584b192f0b1
Jackie Walker <jackieqsuzzy@gmail.com>
2020-03-06 12:09:24 -0700

    finished another css grid practive

---
+++ projects/java-notes/app.js
@@ -0,0 +1,31 @@
+simple/mutable: 
+
+// sting
+"abssldkjfdl"
+
+number 
+2
+2.4
+
+// Boolean
+true or false
+
+complex/immutable:
+
+// object - describing on thing. ex - a user 
+ const obj {
+    name: "chris",
+    age: 3,
+    male: true,
+    friends: [
+        name: "maria",
+        pets: [dogs]
+    ]
+}
+
+
+const numbers = [4, 2, 5, 7, 8]
+
+console.long(numbers [3])
+
+array 
