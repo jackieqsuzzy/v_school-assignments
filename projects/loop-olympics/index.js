7eba853ed74f1c1f3afd25d6c69c3587940f2619
Jackie Walker <jackieqsuzzy@gmail.com>
2020-03-23 20:24:51 -0600

    finished loop olympcs

---
+++ projects/loop-olympics/index.js
@@ -0,0 +1,82 @@
+// Preliminaries
+
+
+// for(var i = 0; i < 9; i++){
+//     console.log([i])
+// }
+
+// for(var i = 10; i >= 0; i--){
+//     console.log([i])
+// }
+
+// var fruit = ["banana", "orange", "apple", "kiwi"]
+
+// for (var i = 0; i < fruit.length; i++){
+//     console.log(fruit[i])
+// }
+
+// Bronze Metal 
+
+// var myArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// for(var i = 0; i < myArray.length; i++){
+//     console.log(myArray[i])
+// }
+
+// or .....
+// for (var i = 0; i < 10; i++){
+//     console.log(i)
+// }
+// for(var i = 9; i > 0; i--){
+//     console.log(i)
+// }
+
+// var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+// for(var i = 0; i < fruit.length; i += 2){
+//     console.log(fruit[i])
+// }
+// Silver Medal
+
+// var peopleArray = [
+//     {
+//     name: "Harrison Ford",
+//     occupation: "Actor"
+//     },
+//     {
+//     name: "Justin Bieber",
+//     occupation: "Singer"
+//     },
+//     {
+//     name: "Vladimir Putin",
+//     occupation: "Politician"
+//     },
+//     {
+//     name: "Oprah",
+//     occupation: "Entertainer"
+//     }
+// ]
+// for (var i = 0; i < peopleArray.length; i++){
+//     console.log(peopleArray[i].name)
+// }
+
+// for (var i = 0; i < peopleArray.length; i++){
+//     console.log(peopleArray[i])
+// }
+// const names = []
+// const occupations = []
+// for (var i = 0; i < peopleArray.length; i++){
+//     if (i % 2 === 0){
+//         names.push(peopleArray[i].name)
+
+//     }
+// }
+
+// for (var i = 0; i < peopleArray.length; i++){
+//     if (i % 2 === 1){
+//         occupations.push(peopleArray[i].occupation)
+//     }
+// }
+// console.log(names)
+// console.log(occupations)    
+
--- projects/moms-shopping-list-start-master2/index.js
+++ projects/moms-shopping-list-start-master2/index.js
@@ -2,30 +2,25 @@ const toDoForm = document.addItems
 toDoForm.addEventListener("submit", function (e){
     e.preventDefault ()
     console.log(toDoForm.input1.value)
-// DOM Manipulation
-// 1. creat or select the new items (lines 9 & 10)
+
 
     const newLi = document.createElement("list")
     const newDiv = document.createElement("div")
     const deleteButton = document.createElement("button")
-    
 
-//  2. Edit the elements 
     newDiv.textContent = toDoForm.input1.value
-    deleteButton.textContent = "delete"
+    deleteButton.textContent = "X"
     deleteButton.addEventListener("click", function(){
         newLi.remove()
     })
-//  3. append the elements to the DOM
     newLi.appendChild(newDiv)
-    document.getElementById('list').appendChild(newLi)
+    document.getElementById("list").appendChild(newLi)
     newLi.appendChild(deleteButton)
 })
 
+// DOM Manipulation
+// 1. creat or select the new items (lines 9 & 10)
 
+//  2. Edit the elements 
 
-
-
-
-
-
+//  3. append the elements to the DOM
