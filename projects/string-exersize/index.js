068aaa87c1e571ce6c115590e000a7b2aefa3b86
Jackie Walker <jackieqsuzzy@gmail.com>
2020-03-26 21:04:51 -0600

    finished assigment

---
+++ projects/string-exersize/index.js
@@ -0,0 +1,24 @@
+// var name = "jackie"
+
+// var uppercasedJackie = name.toUpperCase()
+// console.log(uppercasedJackie)
+
+// var lowercasedJackie = name.toLowerCase()
+// console.log(lowercasedJackie)
+
+
+
+// var num1 = Math.floor(32.9 / 2);
+// console.log(num1)
+
+// var newFunc = "hello"
+// var newFunchalf = newFunc.slice(0, 3)
+// console.log(newFunchalf)
+
+// var newFuncy = "Hello World"
+// var scndFuncy = newFuncy.substr(0, 5)
+// console.log(scndFuncy)
+
+var returnFirstHalf = ("Hello")
+var capFirstHalf = returnFirstHalf.substring(0, 3).toUpperCase() + returnFirstHalf.substring(3)
+console.log(capFirstHalf)
