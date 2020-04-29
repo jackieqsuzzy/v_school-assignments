ebb75c3aef583e50899dea2e300636041c71c3e3
Jackie Walker <jackieqsuzzy@gmail.com>
2020-04-29 16:17:10 -0600

    finished travel form

--- projects-module2/travel-form/index.js
+++ projects-module2/travel-form/index.js
@@ -2,4 +2,25 @@ const form = document.myForm
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
+    const checkedInputs = []
+    for (let i = 0; i < form.food.length; i++){
+        if(form.food[i].checked){
+        checkedInputs.push(form.food[i].value)
+        }
+    }
+    const firstName = form.firstName.value
+    const lastName = form.lastName.value
+    const age = form.age.value
+    const gender = form.gender.value
+    const location = form.location.value
+    const food = form.food.value
+alert(
+`   First name: ${firstName} 
+    Last Name: ${lastName}
+    Age: ${age}
+    Gender: ${gender}
+    Location: ${location}
+    Dietary Restrictions: ${checkedInputs}`)
 })
+
+console.log(form.location.value)
